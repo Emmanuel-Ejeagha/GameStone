f9d83a0219968c328cf74c8a1bff4dd1557502fb
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-21 13:56:07 +0100

    Add GameEndpoints

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3175f9b2f82e32c86e295a999274da11cedf6002")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2aba6d1aeedd1fbfca345b9852b35b6ee4df43d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
