6cf50bbd9d81fbb007db74c0bd8d7402cfb045fe
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-22 11:13:57 +0100

    Add: minimal API

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -9,21 +9,21 @@ public static class GamesEndpoints
     const string GetGameEndpointName = "GetGame";
 
     private static readonly List<GameDto> games = [
-        new (
+        new(
         1,
         "Street Fighter II",
         "Fighting",
         19.99M,
         new DateOnly(1992, 7, 15)
         ),
-    new (
+        new(
             2,
             "Final Fantasy XIV",
             "Roleplaying",
             59.99M,
             new DateOnly(2010, 9, 30)
         ),
-    new (
+        new(
             3,
             "FIFA 23",
             "Sports",
@@ -32,13 +32,14 @@ public static class GamesEndpoints
         )
     ];
 
-    public static WebApplication MapGamesEndpoints(this WebApplication app)
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
     {
+        var group = app.MapGroup("games");
         // GET /games
-        app.MapGet("games", () => games);
+        group.MapGet("/", () => games);
 
         // GET /games/1
-        app.MapGet("games/{id}", (int id) => 
+        group.MapGet("/{id}", (int id) =>
         {
             GameDto? game = games.Find(game => game.Id == id);
             return game is null ? Results.NotFound() : Results.Ok(game);
@@ -46,7 +47,8 @@ public static class GamesEndpoints
         .WithName(GetGameEndpointName);
 
         // POST /games
-        app.MapPost("games", (CreateGameDto newGame) => {
+        group.MapPost("/", (CreateGameDto newGame) =>
+        {
             GameDto game = new(
                 games.Count + 1,
                 newGame.Name,
@@ -56,11 +58,12 @@ public static class GamesEndpoints
             );
             games.Add(game);
 
-            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
-        });
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+        })
+        .WithParameterValidation();
 
         // PUT /games
-        app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
         {
             var index = games.FindIndex(game => game.Id == id);
             if (index == -1)
@@ -78,11 +81,11 @@ public static class GamesEndpoints
         });
 
         // DELETE /games/1
-        app.MapDelete("games/{id}", (int id) => 
+        group.MapDelete("/{id}", (int id) =>
         {
             games.RemoveAll(game => game.Id == id);
             return Results.NoContent();
         });
-        return app;
+        return group;
     }
 }
